f7efcdaf01837ec6d62d850f8903004f60b84042
tregelacio <tre.gelacio@gmail.com>
2022-10-27 21:20:10 -1000

    updated context.js and default.js

--- src/components/context.js
+++ src/components/context.js
@@ -75,12 +75,39 @@ class ProductProvider extends Component {
     }
 
     increment = (id) => {
-        console.log('this is incremnet method');
-    }
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id ===id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+        this.setState(() => {
+            return {
+                cart: [...this.tempCart]
+            }
+        }, () => {
+            this.addTotals();
+        });
+    };
 
     decrement = (id) => {
-        console.log('this is decrement method');
-    }
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id ===id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count - 1;
+        if (product.count === 0) {
+            this.removeItem(id);
+        } else {
+            product.total = product.count * product.price;
+            this.setState(() => {
+                return { cart: [...tempCart] };
+            },
+            () => {
+                this.addTotals();
+            });
+        }
+    };
 
     removeItem = (id) => {
         let tempProducts = [...this.state.products];
@@ -115,7 +142,7 @@ class ProductProvider extends Component {
                 this.addTotals();
             }
         );
-    }
+    };
 
     addTotals = () => {
         let subTotal = 0;
